7f0d4a0fa791f53aeed5554754ca90d05c43216f
DmitryDol <ddimapalih@gmail.com>
2026-04-25 00:17:22 +0400

    first commit

---
+++ lab_3/scripts/setup.py
@@ -0,0 +1,11 @@
+import numpy
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+extensions = [
+    Extension("im2col_cython", ["im2col_cython.pyx"], include_dirs=[numpy.get_include()]),
+]
+
+setup(
+    ext_modules=cythonize(extensions),
+)
